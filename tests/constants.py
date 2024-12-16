73e4baa0b982797b9cb99798bede5201c62abba6
Aleksey3107 <alekseyshka666@gmail.com>
2024-12-16 22:02:57 +0400

    refactor according to linter

--- tests/constants.py
+++ tests/constants.py
@@ -2,13 +2,13 @@ from dataclasses import dataclass
 from enum import Enum
 
 
-class Locators(Enum):
+class DataTestId(Enum):
     CREATE_NEW_FILE_BTN = "create-new-file-btn"
     CRYPTO_ALGO_DROPDOWN = "[data-testid='crypto-algo-selector']"
     AES_KEY_EXTRACTABILITY_DROPDOWN = (
         "//select[@data-testid='aes-key-extractability-selector']"
     )
-    PASSWORD_FIELDS = "//input[@data-testid='aes-passphrase-input']"
+    PASSWORD_FIELD = "//input[@data-testid='aes-passphrase-input']"
     ENCRYPT_BTN = "crypto-btn-encrypt"
     TEXT_TO_ENCRYPT_TEXTAREA = "//textarea[@data-testid='file-editor-mode-text-input']"
     SUMMARY_BTN = "//summary[text()='Details']"
--- tests/cryptic_page.py
+++ tests/cryptic_page.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import Page
 
-from tests.constants import EncryptionInfo, Locators
+from tests.constants import EncryptionInfo, DataTestId
 
 
 class CrypticPage(Page):
@@ -17,52 +17,52 @@ class CrypticPage(Page):
         return self
 
     def click_create_new_button(self):
-        self.click_button(Locators.CREATE_NEW_FILE_BTN.value)
+        self.click_button(DataTestId.CREATE_NEW_FILE_BTN.value)
         return self
 
     def select_crypto_algorithm(self, algorithm: str):
-        self.page.select_option(Locators.CRYPTO_ALGO_DROPDOWN.value, value=algorithm)
+        self.page.select_option(DataTestId.CRYPTO_ALGO_DROPDOWN.value, value=algorithm)
         return self
 
     def select_aes_key_extractability(self, is_extractable: str):
         self.page.select_option(
-            Locators.AES_KEY_EXTRACTABILITY_DROPDOWN.value, value=is_extractable
+            DataTestId.AES_KEY_EXTRACTABILITY_DROPDOWN.value, value=is_extractable
         )
         return self
 
     def enter_password(self, text: str):
-        self.fill_form(Locators.PASSWORD_FIELDS.value, text=text)
+        self.fill_form(DataTestId.PASSWORD_FIELD.value, text=text)
         return self
 
     def click_encrypt_button(self):
-        self.click_button(Locators.ENCRYPT_BTN.value)
+        self.click_button(DataTestId.ENCRYPT_BTN.value)
         return self
 
     def enter_text_to_encrypt(self, text: str):
-        self.fill_form(Locators.TEXT_TO_ENCRYPT_TEXTAREA.value, text=text)
+        self.fill_form(DataTestId.TEXT_TO_ENCRYPT_TEXTAREA.value, text=text)
 
     def click_details(self):
-        self.page.click(Locators.SUMMARY_BTN.value)
+        self.page.click(DataTestId.SUMMARY_BTN.value)
 
     def click_save_file_button(self):
-        self.click_button(Locators.SAVE_FILE_BTN.value)
+        self.click_button(DataTestId.SAVE_FILE_BTN.value)
 
     def click_reset_button(self):
-        self.click_button(Locators.RESET_BTN.value)
+        self.click_button(DataTestId.RESET_BTN.value)
 
     def get_aes_key(self):
-        return self.page.locator(Locators.AES_KEY_EXTRACTED_FIELD.value).get_attribute(
+        return self.page.locator(DataTestId.AES_KEY_EXTRACTED_FIELD.value).get_attribute(
             "value"
         )
 
     def get_auth_tag(self):
-        return self.page.locator(Locators.AUTH_TAG_FIELD.value).get_attribute("value")
+        return self.page.locator(DataTestId.AUTH_TAG_FIELD.value).get_attribute("value")
 
     def get_iv(self):
-        return self.page.locator(Locators.IV_FIELD.value).get_attribute("value")
+        return self.page.locator(DataTestId.IV_FIELD.value).get_attribute("value")
 
     def get_cyphered_text(self):
-        return self.page.locator(Locators.CYPHERED_TEXT_FIELD.value).get_attribute(
+        return self.page.locator(DataTestId.CYPHERED_TEXT_FIELD.value).get_attribute(
             "value"
         )
 
